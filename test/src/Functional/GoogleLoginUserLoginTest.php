412ab35d2dc3f3c269b55ce92e8b012c733ad96b
Oskar Calvo <calvo.oskar@gmail.com>
2016-10-27 17:10:17 +0200

    * Agrego el nuevo test GoogleLoginUserLoginTest, este test comprueba que el usuario anónimo puede acceder al formulario de login y existe el enlace para autentificarse vía Drupal.

---
+++ test/src/Functional/GoogleLoginUserLoginTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * @file( (GoogleLoginUserLoginTest))
+ * Contains \Drupal\googlelogin\Test\GoogleLoginUserLoginTest
+ */
+
+namespace Drupal\Tests\googlelogin\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+use Drupal\user\Entity\User;
+use Drupal\Core\Url;
+
+/**
+ * Provide some basic test for the test that create googlelogin in the login form
+ *
+ * @group Googlelogin
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
+class GoogleLoginUserLoginTest extends BrowserTestBase {
+
+  /**
+   * @var \Drupal\user\Entity\User.
+   */
+  protected $user;
+
+  /**
+   * Enabled modules
+   */
+  public static $modules = ['googlelogin', 'simpletest','devel','devel_debug_log', 'user'];
+
+  /**
+   * Use the Standard profile to test help implementations of many core modules.
+   */
+  protected $profile = 'standard';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+
+  }
+
+
+  /**
+   * Check that the anonymous user can see the form
+   */
+  public function testGoogleLoginCheckAccessUserLoginForm () {
+
+    $this->anonymousUser = $this->drupalCreateUser([]);
+    // log as anonymous, is the only user that can access to this form.
+    $this->drupalLogin($this->anonymousUser);
+    $this->druaplGet(Url::fromRemote('user.login'));
+    //Check that the anonymous user can access to login form.
+    $this->assertSession()->statusCodeEquals(200);
+
+
+  }
+
+  /**
+   * Check there is a link in the login form
+   */
+  public function testGoogleLoginCheckLinkIsAvailable () {
+
+    $this->anonymousUser = $this->drupalCreateUser([]);
+    // log as anonymous, is the only user that can access to this form.
+    $this->drupalLogin($this->anonymousUser);
+    $this->druaplGet(Url::fromRemote('user.login'));
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains('Google Login');
+
+  }
+
+}
